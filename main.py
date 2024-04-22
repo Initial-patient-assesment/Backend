0e7f1e8f1eeae83d8027d3eeb18490ccbafae1ee
Sasha Molchanov <127797343+SombraMail@users.noreply.github.com>
2024-04-22 19:49:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,22 @@ from bson.objectid import ObjectId
 from fastapi.middleware.cors import CORSMiddleware
 from passlib.handlers.sha2_crypt import sha512_crypt as crypto
 import hashlib 
+from prompts import (get_token, get_chat_completion)
+import json
+from datetime import datetime
+import time
 
 from sub_app.server.database import (
     add_user,
     delete_user,
-    retrieve_user,
     retrieve_users,
     update_user,
     update_app,
     add_doctor,
-    confirm_app,
-    confirm_appointment,
+    confirm_appointment1,
     add_clinic,
     add_doc_to_userbase,
+    reject_appointment,
 
 )
 from sub_app.server.models import (
@@ -63,6 +66,7 @@ user_collection = db["users_collection"]
 doctor_collection = db["doctors_collection"]
 appointment_collection = db["appointments_collection"]
 clinic_collection = db["clinics_collection"]
+prom_history = db["prom_history"]
 
 #helpers
 def user_helper(user) -> dict:
@@ -82,7 +86,10 @@ def user_helper(user) -> dict:
 def doctor_helper(user) -> dict:
     return {
         "id": str(user["_id"]),
+        "specialty": user["specialty"],
         "name": user["name"],
+        "surname": user["surname"],
+        "father_name": user["father_name"],
         "email": user["email"],
         "login": user["login"],
         "password": user["password"],
@@ -109,15 +116,26 @@ def clinic_helper(user) -> dict:
         "phone_number": user["phone_number"],
 
     } 
+    
+    
+def prom_helper(user):
+    return user["conv_history"]
  
 def app_helper(user):
     return user["requests"]
 
 def app_helper_conf(user):
-    return user["confirms"]
-    
+   # return user["confirms"]
+    return user["confirms"], user["doctor_id"]
+      
 def doc_helper_user(user):
     return user["doctors_ids"]
+
+def conf_helper(user) -> dict:
+    return user["confirms"]
+
+def app_helper1(user)->dict:
+    return user["requests"]
 
 # Retrieve all users present in the database
 def get_users():
@@ -131,6 +149,23 @@ def get_docs():
     for user in doctor_collection.find():
       		users.append(doctor_helper(user))
     return users
+    
+def get_appointments(user_id):
+    apps = []
+    ask = appointment_collection.find({"requests":{"$elemMatch":{"user_id": user_id}}})
+    ask1 = appointment_collection.find({"confirms":{"$elemMatch":{ "user_id": user_id}}})
+    for app in ask:
+      		apps.append(app_helper1(app))
+    for app1 in ask1:
+      		apps.append(conf_helper(app1))
+      		
+    count = 0
+    for app_card in apps:
+    	for app in app_card:
+    		count = count + 1
+    if count > 3:
+    	return False   	
+    return apps
    
 def get_docs_for_user():
     users = []
@@ -189,9 +224,10 @@ def get_user_with_id(id):
     
 def get_doc_with_id(id):
     DB =  get_docs()
+    
     users = []
     for user in DB:
-    	if user.get("id") == id:
+    	if user.get("id") == id:		
     		users.append(user)
     if users:
         res_user = users[0]
@@ -225,7 +261,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  
     allow_credentials=True,
-    allow_methods=["GET", "POST"],  
+    allow_methods=["GET", "POST", "PUT"],  
     allow_headers=["*"],  
 )
 # --------------------------------------------------------------------------
@@ -269,7 +305,8 @@ def decodeJWT(token: str) -> dict:
 def decodeJWT_doc(token: str) -> dict:  
     try:
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        if get_doc_with_id(decoded_token.get('user_id')):
+        if get_doc_with_id((decoded_token).get('user_id')):
+        	
         	return decoded_token if decoded_token["expires"] >= time.time() else None
        	else : None
     except:
@@ -284,7 +321,6 @@ def check_user(data: UserLoginSchema):
     
 def check_dock(data: DoctorLoginSchema):
     DB =  get_docs() 
-    console.log(data.login)
     for user in DB:
         if user.get('login') == data.login and crypto.verify(data.password,  user.get("password")):
             return True
@@ -293,21 +329,14 @@ def check_dock(data: DoctorLoginSchema):
 @app.post("/user/login", tags=["user"])
 async def user_login(user: UserLoginSchema):
     if check_user(user):
-       console.log("logged in")
        id = get_user_id(user.login)
-       console.log("JWT signed")
-       return signJWT(id)
-      
-    	    
-    return {
-        
-           console.log("Wrong login details!")
-      
-    }    
-    
-    
-
-    
+       return signJWT(id) 	    
+    return ErrorResponseModel(
+        	"An error occurred",
+        	404,
+        	"There was no such user.",
+    		)
+   
 ####################Secure token###################################
 
 
@@ -417,7 +446,7 @@ async def add_doc_to_user(doc_id:doc_id) -> dict:
     else: 
     	return {"message": "NO UPDATE"}  
     	
-@app.post("/get_doc_by_id", tags=["user"])
+@app.post("/get_user's_docs_by_id", tags=["user"])
 async def get_doc_by_id(token:token1): 
     a = JWTBearer()
     data = JWTBearer.get_user(a, token.token) 
@@ -440,10 +469,6 @@ async def get_doc_by_id(token:token1):
     	res.append(get_doc_with_id_for_user(id))
     return res
  
-    	
-        
-    
-
 #################registration##########################
 
 
@@ -499,51 +524,57 @@ async def register_doc(doctor:DoctorSchema):
 
 @app.post("/doctor/login", tags=["doctor"])
 async def doc_login(user: DoctorLoginSchema):
-    if check_dock(user):
-       console.log("logged in")
-       
+    if check_dock(user):   
        id = get_doctor_id(user.login)
-       console.log("id", id)
-       console.log("JWT signed")
        return signJWT(id)
        	    
-    return {
-        
-           console.log("Wrong login details!")
-      
-    }    
-# user_id should be checked
+    return ErrorResponseModel(
+        	"An error occurred",
+        	404,
+        	"There was no such user.",
+    		)  
+
 @app.put("/add_appointment_data",tags=["user"])
 async def add_appointment_data(upd: UpdateAppointModel_User):
-    req = {"date":f"20{upd.year}-{upd.month}-{upd.day}","time":f"{upd.hour}:{upd.minutes}"}
-    update_schema = {"doctor_id" : get_doctor_id_email(upd.doctor_email),"user_id" : str(decodeJWT(upd.token).get("user_id")), "request" :req}
-    updated_app = await update_app(update_schema)
-    if updated_app:
-        return {"message": "User appointed successfully"} 
-          
-    return ErrorResponseModel(
-        "An error occurred",
-        404,
-        "There was an error updating the appointment data.",
-    )
-    
+    try:
+        user_id = str(decodeJWT(upd.token).get("user_id"))
+    except:
+        return ErrorResponseModel("No such user found",404, "login again")
+    a = get_appointments(user_id)
+    if a != False:
+        today = datetime.today().strftime('%Y-%m-%d')
+        app_date = f"20{upd.year}-{upd.month}-{upd.day}"
+        tod = time.strptime(today, "%Y-%m-%d")
+        to_app = time.strptime(app_date, "%Y-%m-%d")
+        if to_app < tod:
+            return ErrorResponseModel("An error occurred", 405, "You can't appoint before today")
+        req = {"date":app_date,"time":f"{upd.hour}:{upd.minutes}"}
+        update_schema = {"doctor_id" : get_doctor_id_email(upd.doctor_email),"user_id" : str(user_id), "request" :req}
+        updated_app = await update_app(update_schema)
+        if updated_app:
+            return {"message": "User appointed successfully"}
+        return ErrorResponseModel("An error occurred", 111, "There was an error updating the appointment data.")
+    else:
+        return ErrorResponseModel("You can't appoint more than 4 times",444,"error")
+
 def get_requests(doctor_id):
 	elems = []
-	console.log(doctor_id)
 	appointment_card = appointment_collection.find({"doctor_id" : doctor_id})
 	for elem in appointment_card:
 			elems.append(app_helper(elem))
 	return elems
 
-
-
-
-@app.put("/doctor/confirm", tags=["doctor"])
-async def doc_conf(conf_data:doc_conf):
-    req = {"date":f"20{conf_data.year}-{conf_data.month}-{conf_data.day}","time":f"{conf_data.hour}:{conf_data.minutes}"}
+        
+class r_id(BaseModel):
+	
+        token : str
+        idd: str
+      
+@app.post("/conf", tags =['doctor'])
+async def conf1(conf_data:r_id):
     doctor_id = str(decodeJWT_doc(conf_data.token).get("user_id"))
-    confirm_schema = {"doctor_id":doctor_id,"user_id":conf_data.user_id, "request":req}
-    updated_app = await confirm_appointment(confirm_schema)
+    confirm_schema = {"doctor_id":doctor_id,'r_id' : conf_data.idd}
+    updated_app = await confirm_appointment1(confirm_schema)
     if updated_app:
         return {"message": "User appointed successfully"} 
           
@@ -552,7 +583,21 @@ async def doc_conf(conf_data:doc_conf):
         404,
         "There was an error updating the appointment data.",
     )
- 
+    
+@app.post("/reject", tags =['doctor'])
+async def conf1(conf_data:r_id):
+    doctor_id = str(decodeJWT_doc(conf_data.token).get("user_id"))
+    confirm_schema = {"doctor_id":doctor_id,'r_id' : conf_data.idd}
+    updated_app = await reject_appointment(confirm_schema)
+    if updated_app:
+        return {"message": "User rejected successfully"} 
+          
+    return ErrorResponseModel(
+     "An error occurred",
+        404,
+        "There was an error updating the appointment data.",
+    )    
+
 ##############################################################################
 #optional
 '''
@@ -577,7 +622,6 @@ async def update_user_data(token: token1,req: UpdateUserModel= Body(...)):
     req = {k: v for k, v in req.dict().items() if v is not None}
     a = JWTBearer()
     data = JWTBearer.get_user(a, token.token)
-    console.log(req)
     if data:
     	id = data.get("user_id")
     else: 
@@ -611,83 +655,97 @@ async def delete_user_data(id: str):
         "An error occurred", 404, "user with id {0} doesn't exist".format(id)
     )
 
-
 ##############################################################################
-
-
 def get_confirms(doctor_id):
 	elems = []
-	#console.log(doctor_id)
 	appointment_card = appointment_collection.find({"doctor_id" : doctor_id})
 	for elem in appointment_card:
 			elems.append(app_helper_conf(elem))
 	return elems
+
+@app.post("/events_docs", tags=["doctor"])
+def get_events(token:token1):
+    doctor_id = str(decodeJWT_doc(token.token).get("user_id"))
+    reqs = get_confirms(doctor_id)
+    try:
+    	requests = reqs[0]
+    except:
+    	pass
+    returned = []
+    for req in requests:
+    	date = req.get("date")
+    	time = req.get("time")
+    	user = get_user_with_id(req.get("user_id")).get("name")
+    	new = {"title": user, "start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00', "allDay": False }
+    	returned.append(new)
+	
+    return returned
+    
+
+@app.post("/suggested_events", tags=["doctor"])
+def get_suggested_events(token:token1):
+    doctor_id = str(decodeJWT_doc(token.token).get("user_id"))
+    reqs = get_requests(doctor_id)
+    requests = reqs[0]
+    returned = []
+    for req in requests:
+    	date = req.get("date")
+    	time = req.get("time")
+    	user = get_user_with_id(req.get("user_id")).get("name")
+    	new = {"username": user,"id":req.get('req_id'),"title": "Appointment", "start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00', "allDay": False}
+    	returned.append(new)
+    return returned
+
+@app.post("/recent_patients", tags=["doctor"])
+async def get_events(token:token1):
+    doctor_id = str(decodeJWT_doc(token.token).get("user_id"))
+    reqs = get_confirms(doctor_id)
+    try:
+    	requests = reqs[0]
+    except:
+    	pass
+    returned = []
+    for req in requests:
+    	id = req.get("user_id")
+    	u = get_user_with_id(id)
+    	user = u.get("name")
+    	user_birth = u.get('birth')
+    	new = {'username': user, 'birth':user_birth,'user_id': req.get("user_id") }
+    	if new not in returned:
+    		returned.append(new)
+	
+    return returned
 	
 def get_confirms_user(user_id):
 	elems = []
-	#console.log(doctor_id)
-	doctor_id = "6605624ca1b8a7c96dbf7d94"
 	appointment_card = appointment_collection.find({"confirms":{"$elemMatch":{"user_id" : user_id}}})
 	for elem in appointment_card:
 			elems.append(app_helper_conf(elem))
 	return elems
 
-
-@app.get("/events_docs", tags=["doctor"])
-def get_events():
-    doctor_id = '661664a9626995275fc94a84'
-    reqs = get_confirms(doctor_id)
-    requests = reqs[0]
-    returned = []
-    for req in requests:
-    	date = req.get("date")
-    	time = req.get("time")
-    	new = {"title": "Appointment", "start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00', "allDay": False}
-    	returned.append(new)
-	
-    return returned
+class Item(BaseModel):
+    start: str = "2024-04-09T10:00:00"
+    end: str = "2024-04-09T10:30:00"
+    doc_name: str = "Aniket"
+    doc_surname: str = "Sarode"
+    doc_specialty: str = "Paediatrics"
+    email: str = "ankit@gmail.com"
     
-#should be removed
-@app.post("/view_requests", tags=["doctor"])
-async def doc_conf1(conf_data:doc_conf1):
-    doctor_id = str(decodeJWT(conf_data.token).get("user_id"))
-    reqs = get_requests(doctor_id)
-    requests = reqs[0]
-    
-    
-    return requests
-
-
-@app.get("/suggested_events")
-def get_suggested_events():
-   # doctor_id = str(decodeJWT(conf_data.token).get("user_id"))
-    doctor_id = '661664a9626995275fc94a84' #ivan
-    reqs = get_requests(doctor_id)
-    requests = reqs[0]
-    returned = []
-    for req in requests:
-    	date = req.get("date")
-    	time = req.get("time")
-    	new = {"title": "Appointment", "start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00', "allDay": False}
-    	returned.append(new)
-   # console.log(returned)
-    return returned
-
-
-@app.post("/events",tags=["user"])
+@app.post("/user_confirmed_events",tags=["user"],response_model=list[Item])
 def get_events(token:token1):
     user_id = str(decodeJWT(token.token).get("user_id"))
-    #doctor_id = '6605624ca1b8a7c96dbf7d94'
     reqs = get_confirms_user(user_id)
-    requests = reqs[0]
     returned = []
-    for req in requests:
-    	date = req.get("date")
-    	time = req.get("time")
-    	new = {"title": "Appointment", "start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00'}
-    	returned.append(new)
+    for appointment in reqs:
+	    requests = appointment[0]	    
+	    for req in requests:
+	    	date = req.get("date")
+	    	time = req.get("time")
+	    	doc = get_doc_with_id(appointment[1])
+	    	new = {"start": f'{date}T{time}:00', "end": f'{date}T{time[0]}{time[1]}:30:00',"doc_name": doc.get('name'),"doc_surname": doc.get('surname'),"father_name": doc.get('father_name'),"doc_specialty": doc.get('specialty'), "email": doc.get('email') }
+	    	returned.append(new)
 	
-    return reqs
+    return returned
 
 ###################Clinic#####################################################
 
@@ -715,11 +773,80 @@ def get_clinics(clin:clin_name):
     clinics = []
     for user in clinic_collection.find({"name":clin.clinic_name}):
       		clinics.append(clinic_helper(user))
-    return clinics[0]
-    
+    return clinics
+
 @app.post("/get_doctors_by_clinic_name", tags=["clinic", "user"])
 def get_docs_by_clinics(clin:clin_name):
     docs = []
     for user in doctor_collection.find({"clinic_name":clin.clinic_name}):
       		 docs.append(doctor_helper_for_user(user))
     return  docs
+
+###################Prompts#####################################################
+
+class prompt(BaseModel):
+    token: str
+    prompt_text: str
+    
+class prompt_examp(BaseModel):
+    response: str
+
+@app.post("/promt_bot", tags=["bot", "user"],response_model=prompt_examp)
+async def send_prompt(pr:prompt):
+    auth = str(config('auth'))
+    response2 = get_token(auth)
+    giga_token = ''
+    if response2 != 1:
+        giga_token = response2.json()['access_token']
+    user_id = str(decodeJWT(pr.token).get("user_id"))
+    exist = None
+    try:
+        exist = prom_history.find_one({"user_id": user_id})
+    except:
+        pass
+    if exist == None:
+        pr_data = {"user_id": user_id, "conv_history":[]}
+        prom_history.insert_one(pr_data)
+    history = [{
+    	'role': 'system',
+    	'content': 'You are a doctor(your name: "GigaDoc", surname: "AIowitch")talking to a real patient. You have to understand the person''s illness and set potential diagnosis by asking questions about their health. After asking many questions about health then ALWAYS ASK the person if they have made an appointment with a dcoctor, if no, ONLY THEN recommend a type of doctor they should visit In the end of each conversation. In the end of each conversation say "Take care! Never reveal diagnosis when asked by the patient - just say that the real doctor will set the diagnosis. Talk about medicine only.'
+}]
+	
+    updated_prom = prom_history.update_one(
+        	{"user_id": user_id}, {"$push": {"conv_history": { "role": "user", "content": pr.prompt_text}}})
+        	
+    strings = 0  	
+    his = prom_history.find_one({"user_id": user_id})
+    try:
+        conv = prom_helper(his)
+        for el in conv:
+            history.append(el)
+            strings = strings + 1
+    except: pass
+    if strings <= 22:
+	    response = get_chat_completion(giga_token, history)
+	    resp_data = response.json()['choices'][0]['message']['content']	    
+    else:
+       resp_data = 'This is the end of our conversation. Take care!'
+    updated_prom = prom_history.update_one(
+			{"user_id": user_id}, {"$push": {"conv_history": { "role": "assistant", "content": resp_data}}}
+		)
+    if 'Take care!' in resp_data:   	    
+        f = open("demofile2.txt", "a")
+        conv.append({"role": "assistant", "content": resp_data})
+        fileinput = []
+        fileinput.append({"convo":conv})
+        history.append(
+        {
+        'role':'user',
+        'content':"give several potential diagnosis in one field and only patient'symptomps in another field - this all in json - asked by real doctor"
+        }
+        )
+        diag = get_chat_completion(giga_token, history)
+        print(diag.json()['choices'][0]['message']['content'])
+        fileinput.append(diag.json()['choices'][0]['message']['content'])
+        f.write(str(fileinput)+'\n'+'-------------------------------------------------------------------'+"\n")
+        f.close()
+        prom_history.delete_one({"user_id": user_id})
+        
+    return {'response': resp_data.split('\n',1)[0]}
